39210f9acaff3b169623b98a8dac9b484aafa932
waristo <waristo.dev@gmail.com>
2021-01-12 22:59:40 +0900

    url, serializer, migrations test

--- djangoreact/api/views.py
+++ djangoreact/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import WiseSayingSerializer
+from .models import WiseSaying
 
-# Create your views here.
+class WiseSayingView(viewsets.ModelViewSet):
+    serializer_class = WiseSayingSerializer
+    queryset = WiseSaying.objects.all()
--- djangoreact/djangoreact/urls.py
+++ djangoreact/djangoreact/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from rest_framework import routers
+from api import views
 from django.views.generic import TemplateView
+
+router = routers.DefaultRouter()
+router.register('wisesaying', views.WiseSayingView, 'wisesaying')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/', include('api.urls')),
+    path('api/', include(router.urls)),
     re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
