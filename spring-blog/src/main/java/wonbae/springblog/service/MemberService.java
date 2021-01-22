ae9a99e887688a1007e842bcd6a9e8a91c22ce2b
waristo <waristo.dev@gmail.com>
2021-01-22 17:29:17 +0900

    member service include test

--- spring-blog/src/main/java/wonbae/springblog/service/MemberService.java
+++ spring-blog/src/main/java/wonbae/springblog/service/MemberService.java
@@ -1,14 +1,37 @@
-//package wonbae.springblog.service;
-//
-//import wonbae.springblog.domain.Member;
-//import wonbae.springblog.repository.MemberRepository;
-//import wonbae.springblog.repository.MemoryMemberRepository;
-//
-//public class MemberService {
-//    private final MemberRepository memberRepository = new MemoryMemberRepository();
-//    public long join(Member member) {
-//        memberRepository.findByName(member.getName());
-//        memberRepository.save(member);
-//        memberRepository.save(member);
-//    }
-//}
+package wonbae.springblog.service;
+
+import wonbae.springblog.domain.Member;
+import wonbae.springblog.repository.MemberRepository;
+import wonbae.springblog.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(n -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long membrId){
+        return memberRepository.findById(membrId);
+    }
+}
